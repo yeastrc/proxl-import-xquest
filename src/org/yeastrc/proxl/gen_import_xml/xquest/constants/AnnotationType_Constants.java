d3417614dfcbb31645f9038a34b7c3f97ac846ae
danjasuw <danjasuw@users.noreply.github.com>
2016-03-17 14:05:14 -0800

    Add Annotations.

--- src/org/yeastrc/proxl/gen_import_xml/xquest/constants/AnnotationType_Constants.java
+++ src/org/yeastrc/proxl/gen_import_xml/xquest/constants/AnnotationType_Constants.java
@@ -1,13 +1,44 @@
 package org.yeastrc.proxl.gen_import_xml.xquest.constants;
 
-public class AnnotationNames_Constants {
+import java.math.BigDecimal;
+
+public class AnnotationType_Constants {
 
 	//  PSM
 	
 	//  Filterable
 	
+	//  FDR: Smaller better
+	
 	public static final String ANNOTATION_NAME_FDR = "FDR";
-	public static final String ANNOTATION_DESCRIPTION_FDR = "The minimum false discovery among all predictions with this score or better.";
+	public static final String ANNOTATION_DESCRIPTION_FDR = "False Discovery Rate";
+	
+	//  Rank: Smaller better
+
+	public static final String ANNOTATION_NAME_RANK = "Rank";
+	public static final String ANNOTATION_DESCRIPTION_RANK = "PSM Rank for Scan";
+
+	//  Score: Bigger better
+	
+	public static final String ANNOTATION_NAME_XQUEST_SCORE = "Score";
+	public static final String ANNOTATION_DESCRIPTION_XQUEST_SCORE = "XQuest score";
 	
 
+	//  Descriptive
+
+	public static final String ANNOTATION_NAME_OBS_MASS = "Obs. mass";
+	public static final String ANNOTATION_DESCRIPTION_OBS_MASS = "Mass of precursor";
+
+	public static final String ANNOTATION_NAME_CALC_MASS = "Calc. mass";
+	public static final String ANNOTATION_DESCRIPTION_CALC_MASS = "Calculated mass for PSM";
+
+	// NO:  Charge, Obs. m/z
+	
+	
+	//  Default Filter Cutoffs
+
+	public static final BigDecimal PSM_FDR_DEFAULT = new BigDecimal( "0.01" );
+	
+
+	public static final BigDecimal PSM_RANK_DEFAULT = new BigDecimal( "1" );
 }
